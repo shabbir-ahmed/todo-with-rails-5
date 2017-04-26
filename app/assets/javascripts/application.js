0836d5105f2d24d439433b95fa96a5a058f4ca4c
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-26 10:31:29 +0000

    New Update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+		$("#new_user").delay(100).fadeIn(100);
+ 		$("#new_user").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#new_user").delay(100).fadeIn(100);
+ 		$("#new_user").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
